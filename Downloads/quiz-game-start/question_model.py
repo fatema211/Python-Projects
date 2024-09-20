757d60123744806edb69ab5d7817e8b420852ec6
Fatema_Shetu <fatemashetu20@gmail.com>
2024-09-20 23:05:19 +0200

    fisrt commit

---
+++ Downloads/quiz-game-start/question_model.py
@@ -0,0 +1,13 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+#ques1 = Question("hi! ki khobor?", "Valo. Tumar ki khobr")
+#ques2 = Question("Hallo babu, wie gehts?", "Mir geht es gut, und dir?")
+#print(ques1.text)
+#print(ques1.ans)
+#print(ques2.text)
+#print(ques2.ans)
+
